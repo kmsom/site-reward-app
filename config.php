1e4c05de444ea15c1c67ffa12152f631d91e5772
kmsom <79022906+kmsom@users.noreply.github.com>
2026-03-20 13:48:39 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'db.zbeddlxzqqivtjmugaze.supabase.co'; // Host do seu print
+$port = '5432';
+$db   = 'postgres';
+$user = 'postgres';
+$pass = 'SUA_SENHA_RESETADA_AQUI'; 
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ATTR_ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro de conexão: " . $e->getMessage());
+}
+
+function formatarGrana($valor) {
+    return "R$ " . number_format($valor, 2, ',', '.');
+}
+?>
